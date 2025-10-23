3612054d628e30a7ee0b13eb83445b622c627d1c
Khansa sheikh <khansa@khansa.localdomain>
2025-10-23 21:28:45 +0530

    first commit

---
+++ create_table.sql
@@ -0,0 +1,3 @@
+CREATE TABLE students (id SERIAL PRIMARY KEY, name VARCHAR(255), age INT, grade VARCHAR(1), city VARCHAR(255));
+INSERT INTO students (name, age, grade, city) VALUES ...;
+SELECT * FROM students;
